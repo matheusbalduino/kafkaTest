a61e2e1616af063389b39b3e84fa4ded97ed76f5
matheusbalduino <matheus.balduin@gmail.com>
2022-08-24 10:34:45 -0300

    fineshed kafka test

--- consumer/index.js
+++ consumer/index.js
@@ -1,5 +1,5 @@
-console.log("consumer...")
 import Kafka from 'node-rdkafka';
+import eventType from '../eventType.js';
 
 const consumer = Kafka.KafkaConsumer({
   'group.id':'kafka',
@@ -9,6 +9,10 @@ const consumer = Kafka.KafkaConsumer({
 consumer.connect();
 
 consumer.on('ready', () => {
-  console.log('consumer ready...')
-});
+  console.log('consumer ready...');
+  consumer.subscribe(['test']);
+  consumer.consume();
+}).on('data', (data) =>{
+  console.log(`received message: ${eventType.fromBuffer(data.value)}`);
+})
 
--- producer/index.js
+++ producer/index.js
@@ -1,10 +1,30 @@
 console.log("producer...")
 import Kafka from 'node-rdkafka';
+import eventType from '../eventType.js'
 
 const stream = Kafka.Producer.createWriteStream({'metadata.broker.list':'localhost:9092'}, {}, { topic: 'test' });
 
+function getRandomAnimal(){
+  const categories = ['CAT', 'DOG']
+  return categories[Math.floor(Math.random() * categories.length)]
+}
+
+function getRandomNoise(category){
+  if(category === 'CAT'){
+    const categories = ['meow', 'puur']
+    return categories[Math.floor(Math.random() * categories.length)]
+  }
+  else{
+    const categories = ['woof', 'bark']
+    return categories[Math.floor(Math.random() * categories.length)]
+  }
+}
+
 function queueMessage(){
-  const success = stream.write(Buffer.from('hi'));
+  const category = getRandomAnimal();
+  const noise = getRandomNoise(category);
+  const event = { category: category, noise: noise }
+  const success = stream.write(eventType.toBuffer(event));
   if(success) console.log('Message wrote successfully to stream');
   else console.log('something went wrong');
 }
